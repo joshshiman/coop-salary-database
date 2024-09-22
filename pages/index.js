0eabd682dd95db32f6defaa45e7cdbef34f38467
Joshua Shiman <joshshiman@proton.me>
2024-09-21 22:02:50 -0400

    Major test

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export async function getServerSideProps() {
     const url = process.env.VERCEL_URL 
@@ -17,29 +17,18 @@ export async function getServerSideProps() {
 }
 
 export default function Home({ jobs }) {
-    const [searchTerm, setSearchTerm] = useState('');
-
-    const filteredJobs = jobs.filter(job =>
-        job.job_title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        job.company.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
     return (
         <div>
             <h1>Co-op Job Repository</h1>
-            <input
-                type="text"
-                placeholder="Search..."
-                onChange={(e) => setSearchTerm(e.target.value)}
-            />
             <div>
-                {filteredJobs.length === 0 ? (
+                {jobs.length === 0 ? (
                     <p>No jobs available.</p>
                 ) : (
-                    filteredJobs.map((job) => (
+                    jobs.map((job) => (
                         <div key={job.id}>
-                            <h3>{job.job_title}</h3>
+                            <h3>{job.role}</h3>
                             <p>{job.company} - {job.salary}</p>
+                            <p>{job.location}</p>
                         </div>
                     ))
                 )}
